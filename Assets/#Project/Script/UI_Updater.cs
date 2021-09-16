ea4fa06418be1cce657de4f7240212c9476be742
elisabetta-boop <elisabetta.saporetti@yahoo.it>
2021-09-16 16:24:42 +0200

    lifes

---
+++ Assets/#Project/Script/UI_Updater.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class UI_Updater : MonoBehaviour
+{
+    public Image[] lifeImages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int index = 0; index< lifeImages.Length; index++)
+        {
+            lifeImages[index].enabled = index < Levels_Manager.lifes;
+        }   
+    }
+}
